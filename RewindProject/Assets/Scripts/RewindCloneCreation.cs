c6e696f26fcec0f3af4741c3a2fdd3ca39c5c0b7
Oleg <oleg.magarill@gmail.com>
2020-08-02 16:34:40 +0300

    123

---
+++ RewindProject/Assets/Scripts/RewindCloneCreation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewindCloneCreation : MonoBehaviour
+{
+    public GameObject RewindClone;
+    public GameObject Player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Instantiate(RewindClone, Player.transform.position, Quaternion.identity);
+            Debug.Log("R was pressed");
+        }
+    }
+}
