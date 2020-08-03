a68ac4ad584df6b09587c273b6cd7eecdb470ec9
Oleg <oleg.magarill@gmail.com>
2020-08-03 13:53:18 +0300

    RewindCooldown

--- RewindProject/Assets/Scripts/RewindCloneCreation.cs
+++ RewindProject/Assets/Scripts/RewindCloneCreation.cs
@@ -13,11 +13,18 @@ public class RewindCloneCreation : MonoBehaviour
 
     GameObject clone;
 
+    [Range(1,5)]
+    public float RewindClooneCooldown = 1f;
+
+    private bool RewindClooneCooldownCoorutineIsRunning = false;
+
+    private float time;
+
     public bool CloneIsPressingE;
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.R) && clone == null)
+        if(Input.GetKeyDown(KeyCode.R) && clone == null && !RewindClooneCooldownCoorutineIsRunning)
         {
             PlayerPositions = new List<Vector2>(Player.GetComponentInChildren<RecordPlayerPositions>().m_PlayerPositions);
             PlayerEpressing = new List<bool>(Player.GetComponentInChildren<RecordPlayerPositions>().m_PlayerPressE);
@@ -28,6 +35,7 @@ public class RewindCloneCreation : MonoBehaviour
         else if(Input.GetKeyDown(KeyCode.R) && clone != null)
         {
             Destroy(clone);
+            StartCoroutine("RewindClooneCooldownCoorutine");
             PlayerPositions.Clear();
             PlayerEpressing.Clear();
             CloneIsPressingE = false;
@@ -50,10 +58,24 @@ public class RewindCloneCreation : MonoBehaviour
             }
             else
             {
+                StartCoroutine("RewindClooneCooldownCoorutine");
                 Destroy(clone);
                 PlayerEpressing.Clear();
                 PlayerPositions.Clear();
             }
         }
+    }
+
+    IEnumerator RewindClooneCooldownCoorutine()
+    {
+        RewindClooneCooldownCoorutineIsRunning = true;
+        time = RewindClooneCooldown;
+        while (time > 0)
+        {
+            time -= Time.deltaTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        time = 0;
+        RewindClooneCooldownCoorutineIsRunning = false;
     }
 }
