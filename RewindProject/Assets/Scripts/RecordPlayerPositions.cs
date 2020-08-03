7017c5e0ea6d0aecfcc5f1434ddae5844aee3fff
Oleg <oleg.magarill@gmail.com>
2020-08-03 10:05:51 +0300

    RewindCloneNow can activate buttons)

--- RewindProject/Assets/Scripts/RecordPlayerPositions.cs
+++ RewindProject/Assets/Scripts/RecordPlayerPositions.cs
@@ -7,26 +7,66 @@ public class RecordPlayerPositions : MonoBehaviour
     public int RecordFrames = 300;
     public int CurrentRecordedFrames = 0;
 
-    public List<Vector3> m_PlayerPositions;
+    public List<Vector2> m_PlayerPositions;
+    public List<bool> m_PlayerPressE;
 
+    private bool IsPressed;
+    //[SerializeField]
+    //public class InfoPak
+    //{
+    //    public Vector3 Position { get; set; }
+    //    public bool IsEwasPressed { get; set; }
+    //}
 
+    //public List<InfoPak> m_InfoPaks;
 
     private void Start()
     {
 
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.E))
+        {
+            IsPressed = true;
+        }
+        else
+        {
+            IsPressed = false;
+        }
     }
     // Update is called once per frame
     void FixedUpdate()
     {
         if (CurrentRecordedFrames < RecordFrames)
         {
-            m_PlayerPositions.Add(this.transform.position); 
+            m_PlayerPositions.Add(this.transform.position);
+            m_PlayerPressE.Add(IsPressed);
+
+            //var m_InfoPak = new InfoPak();
+            //m_InfoPak.IsEwasPressed = Input.GetKeyDown(KeyCode.E);
+            //m_InfoPak.Position = this.transform.position;
+            //m_InfoPaks.Add(m_InfoPak);
         }
         else
         {
             m_PlayerPositions.RemoveAt(0);
             m_PlayerPositions.TrimExcess();
             m_PlayerPositions.Add(this.transform.position);
+
+            m_PlayerPressE.RemoveAt(0);
+            m_PlayerPositions.TrimExcess();
+            m_PlayerPressE.Add(IsPressed);
+
+            //m_InfoPaks.RemoveAt(0);
+            //m_InfoPaks.TrimExcess();
+            //var m_InfoPak = new InfoPak();
+            //m_InfoPak.IsEwasPressed = Input.GetKeyDown(KeyCode.E);
+            //m_InfoPak.Position = this.transform.position;
+            //m_InfoPaks.Add(m_InfoPak);
+
         }
         CurrentRecordedFrames = m_PlayerPositions.Count;
     }
--- RewindProject/Assets/Scripts/RewindCloneCreation.cs
+++ RewindProject/Assets/Scripts/RewindCloneCreation.cs
@@ -8,16 +8,20 @@ public class RewindCloneCreation : MonoBehaviour
     public GameObject RewindClone;
     public GameObject Player;
 
-    public List<Vector3> PlayerPositions;
+    public List<Vector2> PlayerPositions;
+    public List<bool> PlayerEpressing;
 
     GameObject clone;
+
+    public bool CloneIsPressingE;
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.R) && clone == null)
         {
-            PlayerPositions = new List<Vector3>(Player.GetComponentInChildren<RecordPlayerPositions>().m_PlayerPositions);
-            //PlayerPositions = Player.GetComponentInChildren<RecordPlayerPositions>().m_PlayerPositions;
+            PlayerPositions = new List<Vector2>(Player.GetComponentInChildren<RecordPlayerPositions>().m_PlayerPositions);
+            PlayerEpressing = new List<bool>(Player.GetComponentInChildren<RecordPlayerPositions>().m_PlayerPressE);
+            
             clone = Instantiate(RewindClone, Player.transform.position, Quaternion.identity);
             Debug.Log("R was pressed");
         }
@@ -25,7 +29,10 @@ public class RewindCloneCreation : MonoBehaviour
         {
             Destroy(clone);
             PlayerPositions.Clear();
+            PlayerEpressing.Clear();
+            CloneIsPressingE = false;
         }
+
     }
 
     private void FixedUpdate()
@@ -35,11 +42,17 @@ public class RewindCloneCreation : MonoBehaviour
             if (PlayerPositions.Count > 0)
             {
                 clone.transform.position = PlayerPositions.Last();
+
+                CloneIsPressingE = PlayerEpressing[PlayerEpressing.Count - 1];
+
+                PlayerEpressing.RemoveAt(PlayerEpressing.Count -1);
+               // PlayerEpressing.Remove(PlayerEpressing.l);
                 PlayerPositions.Remove(PlayerPositions.Last());
             }
             else
             {
                 Destroy(clone);
+                PlayerEpressing.Clear();
                 PlayerPositions.Clear();
             }
         }
